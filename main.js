34d93b52a26d28c423b4aa71dee43aef29aba65b
MarcosGad <marcosgad@yahoo.com>
2021-10-30 11:41:04 +0200

    enjoy

---
+++ main.js
@@ -0,0 +1,23 @@
+let nums = document.querySelectorAll(".nums .num");
+let section = document.querySelector(".three");
+let started = false; // Started ? No
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  console.log(goal);
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
